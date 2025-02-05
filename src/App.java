e3fc4e5088123efb807c43015dae0a7f47c0dec2
Olga <157001903+OlgaGarciaQuintana@users.noreply.github.com>
2025-02-05 23:24:23 +0100

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,6 +1,14 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        //OOHH LALA
-        System.out.println("Hello, World!");
+       Scanner entrada = new Scanner(System.in);
+		//Variables:
+		double seguidores;
+		System.out.println("Introduce seguidores:");
+		seguidores = entrada.nextDouble();
+		
+		//Mostrar formato:
+		double mostrar_seguidores = seguidores / 1000;
+		System.out.println(String.format("%.2f", mostrar_seguidores) + "k");
     }
 }
